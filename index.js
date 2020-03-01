2df0c004e4f543e002c3b7bde75c3fa1d3ab7504
Fábio Lira <fabiolira1910@hotmail.com>
2020-03-01 15:58:35 -0300

    login page initial configuration

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ var http =  require("http");
 
 var app =  express();
 var server = http.Server(app);
+var io =  require ("socket.io")(server);
 
 const PORT = 3366
 
 app.get("/", function( req, res ){
-    res.sendFile(__dirname + "/html/index.html");
+    res.sendFile(__dirname + "/html/index/index.html");
 });
+
+app.get("/index.css", function( req, res ){
+    res.sendFile(__dirname + "/html/index/index.css");
+});
+
 
 server.listen(PORT, function() {
     console.log("Watching in port " + PORT + ".");
 });
+
+io.on("connection", function(socket){
+    console.log("A user has connected! ");
+    socket.on("disconnect", function(){
+        console.log("A user has disconnected! ");
+    })
+})
